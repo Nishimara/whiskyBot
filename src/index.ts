f8d14921edc50fa2653a748b243ad14d5b70b0ed
Nishimara <admin@abdulakh.fun>
2023-11-29 23:34:16 +0300

    config read

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("Hello, world!");
+import { readFileSync } from "fs";
+import { iConfig } from "./interfaces";
+
+const token: iConfig = JSON.parse(readFileSync('config.json', 'utf-8'));
