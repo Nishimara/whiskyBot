473498818236589658b3fa6352c16ef88c1f20c2
Vladislav <ghoulyaev@gmail.com>
2023-12-01 01:25:49 +0300

    set amount to Float

--- src/methods/whiskey.ts
+++ src/methods/whiskey.ts
@@ -2,8 +2,8 @@ import { Drank } from "../classes/Drank";
 import { User } from "../classes/User";
 
 export const whiskey = (user: User): Drank => {
-  let now: number = parseFloat((Math.random() * 5 + 0.3).toFixed(1));
-  user.setAmount(now);
-  let response = new Drank(now, user.getAmount());
-  return response;
+    let now: number = parseFloat((Math.random() * 5 + 0.3).toFixed(1));
+    user.setAmount(now);
+    let response = new Drank(now, user.getAmount());
+    return response;
 };
