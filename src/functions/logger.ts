7e98726b918fd1274292f457a2a6e81a9c252425
Nishimara <admin@abdulakh.fun>
2023-12-01 01:14:49 +0300

    added logger func return type

--- src/functions/logger.ts
+++ src/functions/logger.ts
@@ -1,7 +1,7 @@
 import { appendFileSync, existsSync, statSync, writeFileSync } from "fs";
 import { logFile } from "../consts";
 
-export const logger = (message: string, from: string = 'none') => {
+export const logger = (message: string, from: string = 'none'): void => {
     // It's temporary that logging perfoms into file
     // In future logs will be in database.
 
@@ -10,7 +10,7 @@ export const logger = (message: string, from: string = 'none') => {
     const now = new Date(Date.now() + 3600 * 3); // Set the time to UTC+3 Timezone
 
     let seconds: string | number;
-    if (now.getSeconds().toString().length == 1) seconds = '0' + now.getSeconds().toString();
+    if (now.getSeconds().toString().length) seconds = '0' + now.getSeconds().toString();
     else seconds = now.getSeconds();
 
     const date = `[${now.getDay()}${separator}${now.getMonth()}${separator}${now.getFullYear()}] [${now.getHours()}:${now.getMinutes()}:${seconds}]`;
