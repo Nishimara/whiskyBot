ced380be18aeee9ffd842dfef87ab883678b7e15
Nishimara <admin@abdulakh.fun>
2023-12-01 00:15:22 +0300

    logger improvements

--- src/functions/logger.ts
+++ src/functions/logger.ts
@@ -8,12 +8,16 @@ export const logger = (message: string, from: string = 'none') => {
     const separator = '.';
 
     const now = new Date(Date.now() + 3600 * 3); // Set the time to UTC+3 Timezone
-    const date = now.getDay() + separator + now.getMonth() + separator + now.getFullYear() + 
-      '] [' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+
+    let seconds: string | number;
+    if (now.getSeconds().toString().length == 1) seconds = '0' + now.getSeconds().toString();
+    else seconds = now.getSeconds();
+
+    const date = `[${now.getDay()}${separator}${now.getMonth()}${separator}${now.getFullYear()}] [${now.getHours()}:${now.getMinutes()}:${seconds}]`;
 
     if (!existsSync(logFile)) writeFileSync(logFile, '');
     const stat = statSync(logFile);
-    if (!stat.size) message = `[${date}] ${from}: ${message}`;
+    if (!stat.size) message = `${date} ${from}: ${message}`;
     else message = `\n[${date}] ${from}: ${message}`;
 
     return appendFileSync(logFile, message); // TODO: limit file size
