39b28ab4a4110887618ad56f58d14cd8e8004411
Nishimara <admin@abdulakh.fun>
2023-12-03 03:41:22 +0300

    changed let to const

--- src/functions/whiskey.ts
+++ src/functions/whiskey.ts
@@ -2,13 +2,13 @@ import { Drank, User } from '../classes';
 import { cooldown } from '../consts';
 
 export const whiskey = (user: User): Drank => {
-    let cd: number = Date.now() - Number(user.getLastTimeDrank());
+    const cd: number = Date.now() - Number(user.getLastTimeDrank());
 
     if (cd > cooldown) {
-        let now: number = parseFloat((Math.random() * 5 + 0.3).toFixed(1));
+        const now: number = parseFloat((Math.random() * 5 + 0.3).toFixed(1));
 
         user.setAmount(now);
-        let response = new Drank(now, user.getAmount());
+        const response = new Drank(now, user.getAmount());
 
         return response;
     } else {
