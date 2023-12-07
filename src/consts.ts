c08fc958b192f7819e6931ef663291f4c1ef94e9
Nishimara <admin@abdulakh.fun>
2023-12-07 16:19:29 +0300

    move min & max settings to consts

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,10 @@ import { readFileSync } from 'fs';
 interface iConfig {
     token: string;
 };
+interface iRandom {
+    min: number,
+    max: number
+};
 
 export const config: iConfig = JSON.parse(readFileSync('config.json', 'utf-8'));
 
@@ -11,3 +15,7 @@ export const cooldown: number = 60 * 60 * 1000; // in miliseconds
 export const ignoreErrorCodes: number[] = [
     400 // not enough rights to send messages into group
 ];
+export const random: iRandom = {
+    "min": 0.3,
+    "max": 5
+};
