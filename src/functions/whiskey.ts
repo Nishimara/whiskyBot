f8080eba038dd12ade66dc6ca656466296d14b50
HackerLacker <admin@abdulakh.fun>
2023-12-07 16:19:29 +0300

    move min & max settings to consts

--- src/functions/whiskey.ts
+++ src/functions/whiskey.ts
@@ -1,11 +1,11 @@
 import { Drank, User } from '../classes';
-import { cooldown } from '../consts';
+import { cooldown, random } from '../consts';
 
 export const whiskey = (user: User): Drank => {
     const cd: number = Date.now() - Number(user.getLastTimeDrank());
 
     if (cd > cooldown) {
-        const now: number = parseFloat((Math.random() * 5 + 0.3).toFixed(1));
+        const now: number = parseFloat((Math.random() * random.max + random.min).toFixed(1));
 
         user.setAmount(now);
         const response = new Drank(now, user.getAmount());
