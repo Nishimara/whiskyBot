709b4191cdce4e41d9353d50f68670f57d15aa36
Vladislav <ghoulyaev@gmail.com>
2023-12-11 02:46:57 +0300

    rename variables

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ bot.catch((err, ctx) => {
     handler(err, ctx);
 });
 
+//ебать тут.....
 bot.command('whiskey', async (ctx) => {
     // TODO: move all this code to commands/whiskey.ts
     if (ctx.chat.type == 'private') return;
@@ -28,63 +29,64 @@ bot.command('whiskey', async (ctx) => {
     const user = new User(ctx.message.from.id);
 
     await user.init();
-    let message;
-    let withHTML;
+    let message: string;
+    let withHTML: number;
 
-    const drank: Drank = whiskey(user);
-
-    if (drank.now == -1) {
-        if (!drank.cooldown) return;
-
-        if (ctx.message.from.username)
-            message = '@' + ctx.message.from.username;
+    whiskey(user).then((drank: Drank) => {
+        if (drank.drankNow == -1) {
+            if (!drank.cooldown) return;
+    
+            if (ctx.message.from.username)
+                message = '@' + ctx.message.from.username;
+            else {
+                message = `<a href="tg://user?id=${ctx.message.from.id}">${ctx.message.from.first_name}</a>`;
+                withHTML = 1;
+            }
+    
+            message += ` ты уже пил виски недавно! Тебе нужно немного отойти.\nВыпито ${
+                Number((drank.drankAll % 1).toFixed(1)) == 0
+                    ? drank.drankAll.toFixed(0)
+                    : drank.drankAll.toFixed(1)
+            } литров.\nНафармлено ${user.getMoney()} монет.\n\nПопробуй снова через ${(
+                drank.cooldown /
+                (1000 * 60)
+            )
+                .toString()
+                .match(/\d+/)} м. ${((drank.cooldown / 1000) % 60)
+                .toString()
+                .match(/\d+/)} с.`;
+    
+            logger.push(
+                `Cooldown triggered with ms ${drank.cooldown}`,
+                ctx.message.from.id
+            );
+    
+            if (withHTML) ctx.replyWithHTML(message);
+    
+            return ctx.reply(message);
+        }
+    
+        if (ctx.message.from.username) message = '@' + ctx.message.from.username;
         else {
             message = `<a href="tg://user?id=${ctx.message.from.id}">${ctx.message.from.first_name}</a>`;
             withHTML = 1;
         }
-
-        message += ` ты уже пил виски недавно! Тебе нужно немного отойти.\nВыпито ${
-            Number((user.getAmount() % 1).toFixed(1)) == 0
-                ? user.getAmount().toFixed(0)
-                : user.getAmount().toFixed(1)
-        } литров.\nНафармлено ${user.getMoney()} монет.\n\nПопробуй снова через ${(
-            drank.cooldown /
-            (1000 * 60)
-        )
-            .toString()
-            .match(/\d+/)} м. ${((drank.cooldown / 1000) % 60)
-            .toString()
-            .match(/\d+/)} с.`;
-
-        logger.push(
-            `Cooldown triggered with ms ${drank.cooldown}`,
-            ctx.message.from.id
-        );
-
+        message += ` ты выпил ${drank.drankNow} литров виски и заработал ${
+            drank.money
+        } монет, красава. За все время ты бахнул ${drank.drankAll.toFixed(1)} литров`;
+    
+        logger.push(`Added ${drank.drankNow} liters of whisky`, ctx.message.from.id);
+        logger.push(`Added ${drank.money} moneys`, ctx.message.from.id);
+    
         if (withHTML) return ctx.replyWithHTML(message);
-
+    
         return ctx.reply(message);
-    }
+    });
 
-    if (ctx.message.from.username) message = '@' + ctx.message.from.username;
-    else {
-        message = `<a href="tg://user?id=${ctx.message.from.id}">${ctx.message.from.first_name}</a>`;
-        withHTML = 1;
-    }
-    message += ` ты выпил ${drank.now} литров виски и заработал ${
-        drank.money
-    } монет, красава. За все время ты бахнул ${drank.every.toFixed(1)} литров`;
-
-    logger.push(`Added ${drank.now} liters of whisky`, ctx.message.from.id);
-    logger.push(`Added ${drank.money} moneys`, ctx.message.from.id);
-
-    if (withHTML) return ctx.replyWithHTML(message);
-
-    return ctx.reply(message);
 });
 
 bot.command('info', async (ctx) => {
-    await info(ctx);
+    info(ctx);
 });
 
 bot.launch();
