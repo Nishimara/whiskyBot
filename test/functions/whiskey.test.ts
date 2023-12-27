e06f6d55cd2b8784f7b8380b265fc0a486ddf09e
Nishimara <admin@abdulakh.fun>
2023-12-27 23:08:16 +0300

    mock prisma func

--- test/functions/whiskey.test.ts
+++ test/functions/whiskey.test.ts
@@ -2,9 +2,9 @@ import { Drank } from '../../src/classes/Drank';
 import { User } from '../../src/classes/User'; // can't import from 'src/classes' cuz it's for some reason throws an error
 import { whiskey } from '../../src/functions';
 import { random, cooldown } from '../../src/consts';
+import { mock, it, expect } from 'bun:test';
 
 class MockUser extends User {
-
     public setDrankAll(): void {}
 
     public setLastTimeDrank(): void {}
@@ -12,9 +12,18 @@ class MockUser extends User {
     public setMoney(): void {}
 }
 
+mock('@prisma/client', () => ({
+    PrismaClient: jest.fn().mockImplementation(() => ({
+        user: {
+            findMany: jest.fn()
+        }
+    }))
+}));
+
+// todo: spy on func so it cannot access prisma
 it('whiskey', async () => {
     const user = new MockUser(1);
-    const whisk = await whiskey(user);
+    const whisk = await whiskey(user, 0);
 
     expect(whisk).toBeInstanceOf(Drank);
 
@@ -25,6 +34,9 @@ it('whiskey', async () => {
     expect(whisk.drankNow).toBeGreaterThanOrEqual(random.drankMin);
 
     expect(whisk.drankAll).toBe(0);
-    
-    expect(whisk.cooldown! * -1 + cooldown * 2 - Date.now()).toBeCloseTo(cooldown, -1); // why is whisk.cooldown is negative?
+
+    expect(whisk.cooldown! * -1 + cooldown * 2 - Date.now()).toBeCloseTo(
+        cooldown,
+        -4
+    ); // why is whisk.cooldown is negative?
 });
